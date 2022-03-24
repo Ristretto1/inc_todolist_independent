1057b6ff170ee5d53104a2b46c2519c1450c3c31
Antonov Kirill <antonovkir2@gmail.com>
2022-03-24 09:48:17 +0500

    add remove action

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {Button} from './components/Button';
+import {v1} from 'uuid';
+import {TasksList} from './components/TasksList';
+
+export type TaskType = {
+    id: string
+    task: string
+    isDone: boolean
+}
 
 function App() {
+
+    const [tasks, setTasks] = useState<Array<TaskType>>([
+        {id: v1(), task: '1', isDone: false},
+        {id: v1(), task: '2', isDone: false},
+        {id: v1(), task: '3', isDone: true},
+        {id: v1(), task: '4', isDone: true},
+    ])
+
+    const removeTask = (id: string) => setTasks(tasks.filter(t => t.id !== id))
+
     return (
         <div className="App">
             <div>
                 <h3>What to learn</h3>
                 <div>
                     <input/>
-                    <button>+</button>
+                    <Button name={'+'} callback={() => {
+                    }}/>
                 </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
+
+                <TasksList tasks={tasks} removeTask={removeTask}/>
+
                 <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
+                    <Button name={'All'} callback={() => {
+                    }}/>
+                    <Button name={'Active'} callback={() => {
+                    }}/>
+                    <Button name={'Completed'} callback={() => {
+                    }}/>
                 </div>
             </div>
         </div>
