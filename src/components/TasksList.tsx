b322bae2ee707bfe3f989dfece249d34213489e7
Antonov Kirill <antonovkir2@gmail.com>
2022-03-24 09:48:06 +0500

    create TasksList comp

---
+++ src/components/TasksList.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Button} from './Button';
+import {TaskType} from '../App';
+
+type TasksListPropsType ={
+    tasks: Array<TaskType>
+    removeTask: (id: string) => void
+}
+
+export const TasksList:React.FC<TasksListPropsType> = ({tasks, removeTask}) => {
+
+    const onRemoveTaskHandler = (id: string) => removeTask(id)
+
+    return (
+        <ul>
+        {tasks.map(t => {
+            return (
+                    <li key={t.id}>
+                        <input
+                            type="checkbox"
+                            checked={t.isDone}
+                        />
+                        <span>{t.task}</span>
+                        <Button name={'x'} callback={() => onRemoveTaskHandler(t.id)}/>
+                    </li>
+            )
+            })}
+        </ul>
+         );
+};
+
