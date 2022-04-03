8b4d9603edfb30b2c33e41a63342c76132947d9d
Antonov Kirill <antonovkir2@gmail.com>
2022-04-03 08:57:30 +0500

    add new comp BTN

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import {Button} from './components/Button';
-import {v1} from 'uuid';
-import {TasksList} from './components/TasksList';
-import {TodoList} from './components/TodoList';
-
-export type TaskType = {
-    id: string
-    task: string
-    isDone: boolean
-}
-export type FilterType = 'all' | 'active' | 'completed'
-
-export type TodolistType = {
-    id: string
-    title: string
-    filter: FilterType
-}
 
 function App() {
-    const removeTask = (id: string, todolistId: string) => {
-        const tasks = tasksOdj[todolistId]
-        const filteredTasks = tasks.filter(t => t.id !== id)
-        tasksOdj[todolistId] = filteredTasks
-        setTasks({...tasksOdj})
-    }
-    const checkboxSwitch = (id: string, done: boolean, todolistId: string) => {
-        const tasks = tasksOdj[todolistId]
-        const mappedTasks = tasks.map(t => t.id === id ? {...t, isDone: done} : t)
-        tasksOdj[todolistId] = mappedTasks
-        setTasks({...tasksOdj})
-    }
-    const addTask = (title: string, todolistId: string) => {
-        const tasks = tasksOdj[todolistId]
-        const task = {id: v1(), task: title, isDone: false}
-        const newTasks = [task, ...tasks]
-        tasksOdj[todolistId] = newTasks
-        setTasks({...tasksOdj})
-    }
-
-    const changeFilter = (filter: FilterType, todolistId: string) => {
-        const todolist = todolists.find(tl => tl.id === todolistId)
-        if (todolist) {
-            todolist.filter = filter
-            setTodolists([...todolists])
-        }
-    }
-
-    const removeTodolist = (TLId: string) => {
-        setTodolists(todolists.filter(tl => tl.id !== TLId))
-        delete tasksOdj[TLId]
-        setTasks({...tasksOdj})
-    }
-
-    const todolist1 = v1()
-    const todolist2 = v1()
-
-    const [todolists, setTodolists] = useState<Array<TodolistType>>([
-        {id: todolist1, title: 'What to learn', filter: 'all'},
-        {id: todolist2, title: 'What to buy', filter: 'all'},
-    ])
-
-    const [tasksOdj, setTasks] = useState({
-        [todolist1]: [
-            {id: v1(), task: '1', isDone: false},
-            {id: v1(), task: '2', isDone: false},
-            {id: v1(), task: '3', isDone: true},
-            {id: v1(), task: '4', isDone: true},
-        ],
-        [todolist2]: [
-            {id: v1(), task: '5', isDone: false},
-            {id: v1(), task: '6', isDone: true},
-            {id: v1(), task: '7', isDone: false},
-            {id: v1(), task: '8', isDone: true},
-        ],
-    })
-
     return (
         <div className="App">
-            {todolists.map(tl => {
-                let filteredTask = tasksOdj[tl.id]
-                if (tl.filter === 'completed') filteredTask = tasksOdj[tl.id].filter(t => t.isDone)
-                if (tl.filter === 'active') filteredTask = tasksOdj[tl.id].filter(t => !t.isDone)
-
-                return (
-                    <TodoList key={tl.id}
-                              TLid={tl.id}
-                              title={tl.title}
-                              addTask={addTask}
-                              tasks={filteredTask}
-                              removeTask={removeTask}
-                              checkboxSwitch={checkboxSwitch}
-                              changeFilter={changeFilter}
-                              filter={tl.filter}
-                              removeTodolist={removeTodolist}
-                    />
-                )
-            })}
-
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,15 +3,14 @@ import React from 'react';
 type ButtonPropsType = {
     name: string
     callback: () => void
-    className?: string
 }
 
-export const Button:React.FC<ButtonPropsType> = ({name, callback, className}) => {
+export const Button: React.FC<ButtonPropsType> = ({name, callback}) => {
 
-    const onClickBtnHandler =()=> callback()
+    const onClickHandler = () => callback()
 
     return (
-        <button onClick={onClickBtnHandler} className={className}>{name}</button>
+        <button onClick={onClickHandler}>{name}</button>
     );
 };
 
